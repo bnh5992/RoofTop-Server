1966413b17fae301e567b6eb9d716f5eb51c93a1
bnh5992 <skagus159632@naver.com>
2024-05-07 14:04:06 +0900

    init spring

---
+++ src/main/kotlin/com/rooptop_server/RoopTopServerApplication.kt
@@ -0,0 +1,11 @@
+package com.rooptop_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoopTopServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<RoopTopServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/rooptop_server/RoopTopServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rooptop_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RoopTopServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
