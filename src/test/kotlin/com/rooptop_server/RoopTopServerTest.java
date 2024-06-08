fc4e14f99c0cd72c66799bb11774c9b3eb23dfc3
bnh5992 <skagus159632@naver.com>
2024-06-08 13:30:07 +0900

    test

---
+++ src/test/kotlin/com/rooptop_server/RoopTopServerTest.java
@@ -0,0 +1,11 @@
+package com.rooptop_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = RoopTopServerTest.class)
+    class RoopTopServerTest {
+    @Test
+    void contextLoads() {
+    }
+}
